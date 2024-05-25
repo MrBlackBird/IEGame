6659f45f50278c4c3454887d7c3af478b3482603
Raven <michal.szerechowicz@gmail.com>
2024-05-25 13:17:25 +0200

    restructuring

--- GameObject/player/object.hpp
+++
@@ -1,9 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-
-class Object : public sf::Drawable {
-public:
-  virtual void draw(sf::RenderTarget &target,
-                    sf::RenderStates states) const override = 0;
-  virtual ~Object() = default;
-};
